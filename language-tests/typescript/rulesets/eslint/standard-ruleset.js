e07c5571ee1225f09b12337760b336577cb91a29
Marco Venanzoni <marco.venanzoni@audibene.de>
2025-05-25 22:47:15 +0300

    Convert the ruleset to flat config format

--- language-tests/typescript/rulesets/eslint/standard-ruleset.js
+++ language-tests/typescript/rulesets/eslint/standard-ruleset.js
@@ -1,35 +1,46 @@
-/**
- * TypeScript ESLint ruleset using simple legacy format
- * Based on official typescript-eslint documentation
- */
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
-  rules: {
-    // Basic ESLint rules
-    "no-console": "warn",
-    "no-eval": "error",
-    "no-alert": "error",
-    "no-debugger": "error",
-    "prefer-const": "error",
-    "no-var": "error",
-    eqeqeq: "error",
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import js from "@eslint/js"
 
-    // Disable base rules that have TypeScript equivalents
-    "no-unused-vars": "off",
-    "no-undef": "off",
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": typescriptEslint,
+    },
+    rules: {
+      // Basic ESLint rules
+      "no-console": "warn",
+      "no-eval": "error",
+      "no-alert": "error",
+      "no-debugger": "error",
+      "prefer-const": "error",
+      "no-var": "error",
+      eqeqeq: "error",
 
-    // TypeScript-specific rules
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/explicit-function-return-type": "warn",
-    "@typescript-eslint/prefer-const": "error",
-    "@typescript-eslint/ban-types": "error",
-    "@typescript-eslint/no-non-null-assertion": "warn",
-    "@typescript-eslint/prefer-nullish-coalescing": "error",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-misused-promises": "error",
-    "@typescript-eslint/prefer-readonly": "warn",
+      // Disable base rules that have TypeScript equivalents
+      "no-unused-vars": "off",
+      "no-undef": "off",
+
+      // TypeScript-specific rules
+      "@typescript-eslint/no-unused-vars": "error",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "warn",
+      "@typescript-eslint/prefer-const": "error",
+      "@typescript-eslint/ban-types": "error",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/prefer-nullish-coalescing": "error",
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": "error",
+      "@typescript-eslint/prefer-readonly": "warn",
+    },
   },
-}
+]
