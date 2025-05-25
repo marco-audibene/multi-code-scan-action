bfd38b1c3aba7027126368a8459327ff0bcbc7ca
Marco Venanzoni <marco.venanzoni@audibene.de>
2025-05-26 01:12:10 +0300

    back to no typescript

--- language-tests/typescript/rulesets/eslint/standard-ruleset.js
+++ language-tests/typescript/rulesets/eslint/standard-ruleset.js
@@ -1,25 +1,19 @@
 /**
- * TypeScript ESLint Configuration (Legacy Format)
- * https://typescript-eslint.io/getting-started
+ * TypeScript ESLint Configuration (Flat Config Format - No imports)
+ * Using basic ESLint rules without any package imports
  */
-module.exports = {
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
-  parserOptions: {
+export default {
+  languageOptions: {
     ecmaVersion: 2021,
     sourceType: "module",
-    ecmaFeatures: {
-      jsx: true,
+    globals: {
+      browser: true,
+      es2021: true,
+      node: true,
     },
   },
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
   rules: {
-    // Basic ESLint rules
+    // Basic ESLint rules (manually specified)
     "no-console": "warn",
     "no-eval": "error",
     "no-alert": "error",
@@ -27,15 +21,7 @@ module.exports = {
     "prefer-const": "error",
     "no-var": "error",
     eqeqeq: "error",
-
-    // TypeScript-specific rules
-    "@typescript-eslint/no-unused-vars": "error",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/explicit-function-return-type": "warn",
-    "@typescript-eslint/no-non-null-assertion": "warn",
-    "@typescript-eslint/prefer-nullish-coalescing": "error",
-    "@typescript-eslint/no-floating-promises": "error",
-    "@typescript-eslint/no-misused-promises": "error",
-    "@typescript-eslint/ban-types": "error",
+    "no-unused-vars": "off", // Disabled for TypeScript
+    "no-undef": "off", // Disabled for TypeScript
   },
 }
